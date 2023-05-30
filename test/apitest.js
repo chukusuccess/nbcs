6f5253d81043f66dce4bf7cf659b151b54886364
Papa Birahim Seye <pby@ivalua.com>
2023-05-30 17:53:50 +0200

    Testing API requests using Axios #3

--- test/apitest.js
+++ test/apitest.js
@@ -4,32 +4,36 @@ import { stringify } from "postcss";
 
 let promises = [];
 let names = [];
+let id = 3532085;
 
-async function GetDataByBhid(id, output) {
+axiosRetry(axios, { retries: 5 })
+// Exponential back-off retry delay between requests
+axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
 
-    let brawlhallaApi = axios.create({ baseURL: "https://brawlhalla.fly.dev/v1" });
+let brawlhallaApi = axios.create({ baseURL: "https://brawlhalla.fly.dev/v1" });
 
-    axiosRetry(brawlhallaApi, { retries: 5 })
+function GetDataByBhid(id, output) {
 
-    return brawlhallaApi.get(`/stats/id?brawlhalla_id=${id}`)
-        .catch(err => { console.log("Erreur") })
-        .then(response => {
-            // console.log(response.statusText)
-            console.log(response.data.data.name)
-            
-        }
-        )
+	let brawlhallaApi = axios.create({ baseURL: "https://brawlhalla.fly.dev/v1" });
+
+	return brawlhallaApi.get(`/stats/id?brawlhalla_id=${id}`)
+		// .catch(function (error) {
+		// 	console.log(error.toJSON());
+		// })
+		// .then(function (response) {
+		// 	console.log(response.data.data.name)
+		// }
+		// )
 }
 
 
 // var request = axios.create({ baseURL: "https://brawlhalla.fly.dev/v1" });
 
-axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
+// axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
 
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 2; i++) {
     // ID: 3532085
     promises.push(GetDataByBhid(3532085 + i));
 };
 
-// Promise.all(promises).then(values => {console.log(values)})
-
+Promise.all(promises).then(values => {values.forEach(element => console.log(element.data.data.name))})
